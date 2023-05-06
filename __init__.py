83aa31e67033bc4ba142b5bcc887e683c82376ff
ДанСтат Консультирование <85887371+datstat-consulting@users.noreply.github.com>
2023-05-06 23:47:30 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
+"""
+PyRPO: A Python package for Robust Portfolio Optimization.
+
+This package provides an implementation of Robust Portfolio Optimization
+based on the ellipsoid uncertainty set, allowing users to find optimal
+portfolio weights considering estimation errors in expected returns and
+covariance matrix.
+"""
+
 from .PyRPO import PyRPO
+
+__all__ = ['PyRPO']
